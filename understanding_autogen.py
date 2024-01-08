bbecf4c6ae73b9ed08f642047f4c2da0a3c5c62f
SHIV-anna <shivshankar07123@gmail.com>
2024-01-08 10:38:40 +0530

    added understanding_autogen.py file

---
+++ understanding_autogen.py
@@ -0,0 +1,37 @@
+import autogen
+
+config_list_gpt35 = autogen.config_list_from_json(
+    "/content/drive/MyDrive/OAI_CONFIG_LIST",
+    filter_dict={
+        "model": {
+            "gpt-3.5-turbo",
+            "gpt-3.5-turbo-16k",
+            "gpt-3.5-turbo-0301",
+            "chatgpt-35-turbo-0301",
+            "gpt-35-turbo-v0301",
+        },
+    },
+)
+
+
+llm_config = {"config_list": config_list_gpt35, "cache_seed": 42}
+user_proxy = autogen.UserProxyAgent(
+   name="User_proxy",
+   system_message="A human admin.",
+   code_execution_config={"last_n_messages": 2, "work_dir": "groupchat"},
+   human_input_mode="TERMINATE"
+)
+coder = autogen.AssistantAgent(
+    name="Coder",
+    llm_config=llm_config,
+)
+pm = autogen.AssistantAgent(
+    name="Product_manager",
+    system_message="Creative in software product ideas.",
+    llm_config=llm_config,
+)
+groupchat = autogen.GroupChat(agents=[user_proxy, coder, pm], messages=[], max_round=12)
+manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
+
+user_proxy.initiate_chat(manager, message="Find a latest paper about gpt-4 on arxiv and find its potential applications in software.")
+# type exit to terminate the chat
