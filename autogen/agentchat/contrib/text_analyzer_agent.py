9d8ce124f0f84abd411ec1c326a2f3d87dcfbbd1
SHIV-anna <shivshankar07123@gmail.com>
2024-01-24 22:36:26 +0530

    added print statements

--- autogen/agentchat/contrib/text_analyzer_agent.py
+++ autogen/agentchat/contrib/text_analyzer_agent.py
@@ -67,7 +67,7 @@ class TextAnalyzerAgent(ConversableAgent):
             [analysis_instructions, text_to_analyze, analysis_instructions]
         )  # Repeat the instructions.
         # Generate and return the analysis string.
-        print(f"text_to_analyze: {text_to_analyze}")
-        print(f"analysis_instructions: {analysis_instructions}")
-        print(f"msg_text: {msg_text}")
+        # print(f"text_to_analyze: {text_to_analyze}")
+        # print(f"analysis_instructions: {analysis_instructions}")
+        # print(f"msg_text: {msg_text}")
         return self.generate_oai_reply([{"role": "user", "content": msg_text}], None, None)[1]
