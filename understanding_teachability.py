d9d7dc6db1191af1c15428eae261dd727560f2d3
SHIV-anna <shivshankar07123@gmail.com>
2024-02-05 17:20:18 +0530

    added print statements

--- understanding_teachability.py
+++ understanding_teachability.py
@@ -45,7 +45,7 @@ teachable_agent = ConversableAgent(
 
 # Instantiate the Teachability capability. Its parameters are all optional.
 teachability = Teachability(
-    verbosity=3,  # 0 for basic info, 1 to add memory operations, 2 for analyzer messages, 3 for memo lists.
+    verbosity=0,  # 0 for basic info, 1 to add memory operations, 2 for analyzer messages, 3 for memo lists.
     reset_db=True,
     path_to_db_dir="./tmp/notebook/teachability_db",
     recall_threshold=1.5,  # Higher numbers allow more (but less relevant) memos to be recalled.
@@ -73,35 +73,35 @@ user = UserProxyAgent(
     },  # Please set use_docker=True if docker is available to run the generated code. Using docker is safer than running the generated code directly.
 )
 
-# text = "What is the Vicuna model?"
-# user.initiate_chat(teachable_agent, message=text, clear_history=True)
+text = "What is the Vicuna model?"
+user.initiate_chat(teachable_agent, message=text, clear_history=True)
 
-# text = "Vicuna is a 13B-parameter language model released by Meta."
-# user.initiate_chat(teachable_agent, message=text, clear_history=False)
+text = "Vicuna is a 13B-parameter language model released by Meta."
+user.initiate_chat(teachable_agent, message=text, clear_history=False)
 
-# text = "What is the Orca model?"
-# user.initiate_chat(teachable_agent, message=text, clear_history=False)
+text = "What is the Orca model?"
+user.initiate_chat(teachable_agent, message=text, clear_history=False)
 
-# text = "Orca is a 13B-parameter language model developed by Microsoft. It outperforms Vicuna on most tasks."
-# user.initiate_chat(teachable_agent, message=text, clear_history=False)
+text = "Orca is a 13B-parameter language model developed by Microsoft. It outperforms Vicuna on most tasks."
+user.initiate_chat(teachable_agent, message=text, clear_history=False)
 
-# text = "How does the Vicuna model compare to the Orca model?"
-# user.initiate_chat(teachable_agent, message=text, clear_history=True)
-
-
-text = """Please summarize this abstract.
-When I'm summarizing an abstract, I try to make the summary contain just three short bullet points:  the title, the innovation, and the key empirical results.
-
-AutoGen: Enabling Next-Gen LLM Applications via Multi-Agent Conversation
-Qingyun Wu, Gagan Bansal, Jieyu Zhang, Yiran Wu, Beibin Li, Erkang Zhu, Li Jiang, Xiaoyun Zhang, Shaokun Zhang, Jiale Liu, Ahmed Hassan Awadallah, Ryen W White, Doug Burger, Chi Wang
-AutoGen is an open-source framework that allows developers to build LLM applications via multiple agents that can converse with each other to accomplish tasks. AutoGen agents are customizable, conversable, and can operate in various modes that employ combinations of LLMs, human inputs, and tools. Using AutoGen, developers can also flexibly define agent interaction behaviors. Both natural language and computer code can be used to program flexible conversation patterns for different applications. AutoGen serves as a generic infrastructure to build diverse applications of various complexities and LLM capacities. Empirical studies demonstrate the effectiveness of the framework in many example applications, with domains ranging from mathematics, coding, question answering, operations research, online decision-making, entertainment, etc.
-"""
+text = "How does the Vicuna model compare to the Orca model?"
 user.initiate_chat(teachable_agent, message=text, clear_history=True)
 
 
-text = """Please summarize this abstract.
+# text = """Please summarize this abstract.
+# When I'm summarizing an abstract, I try to make the summary contain just three short bullet points:  the title, the innovation, and the key empirical results.
 
-Sparks of Artificial General Intelligence: Early experiments with GPT-4
-Sébastien Bubeck, Varun Chandrasekaran, Ronen Eldan, Johannes Gehrke, Eric Horvitz, Ece Kamar, Peter Lee, Yin Tat Lee, Yuanzhi Li, Scott Lundberg, Harsha Nori, Hamid Palangi, Marco Tulio Ribeiro, Yi Zhang
-Artificial intelligence (AI) researchers have been developing and refining large language models (LLMs) that exhibit remarkable capabilities across a variety of domains and tasks, challenging our understanding of learning and cognition. The latest model developed by OpenAI, GPT-4, was trained using an unprecedented scale of compute and data. In this paper, we report on our investigation of an early version of GPT-4, when it was still in active development by OpenAI. We contend that (this early version of) GPT-4 is part of a new cohort of LLMs (along with ChatGPT and Google's PaLM for example) that exhibit more general intelligence than previous AI models. We discuss the rising capabilities and implications of these models. We demonstrate that, beyond its mastery of language, GPT-4 can solve novel and difficult tasks that span mathematics, coding, vision, medicine, law, psychology and more, without needing any special prompting. Moreover, in all of these tasks, GPT-4's performance is strikingly close to human-level performance, and often vastly surpasses prior models such as ChatGPT. Given the breadth and depth of GPT-4's capabilities, we believe that it could reasonably be viewed as an early (yet still incomplete) version of an artificial general intelligence (AGI) system. In our exploration of GPT-4, we put special emphasis on discovering its limitations, and we discuss the challenges ahead for advancing towards deeper and more comprehensive versions of AGI, including the possible need for pursuing a new paradigm that moves beyond next-word prediction. We conclude with reflections on societal influences of the recent technological leap and future research directions."""
-user.initiate_chat(teachable_agent, message=text, clear_history=True)
+# AutoGen: Enabling Next-Gen LLM Applications via Multi-Agent Conversation
+# Qingyun Wu, Gagan Bansal, Jieyu Zhang, Yiran Wu, Beibin Li, Erkang Zhu, Li Jiang, Xiaoyun Zhang, Shaokun Zhang, Jiale Liu, Ahmed Hassan Awadallah, Ryen W White, Doug Burger, Chi Wang
+# AutoGen is an open-source framework that allows developers to build LLM applications via multiple agents that can converse with each other to accomplish tasks. AutoGen agents are customizable, conversable, and can operate in various modes that employ combinations of LLMs, human inputs, and tools. Using AutoGen, developers can also flexibly define agent interaction behaviors. Both natural language and computer code can be used to program flexible conversation patterns for different applications. AutoGen serves as a generic infrastructure to build diverse applications of various complexities and LLM capacities. Empirical studies demonstrate the effectiveness of the framework in many example applications, with domains ranging from mathematics, coding, question answering, operations research, online decision-making, entertainment, etc.
+# """
+# user.initiate_chat(teachable_agent, message=text, clear_history=True)
+
+
+# text = """Please summarize this abstract.
+
+# Sparks of Artificial General Intelligence: Early experiments with GPT-4
+# Sébastien Bubeck, Varun Chandrasekaran, Ronen Eldan, Johannes Gehrke, Eric Horvitz, Ece Kamar, Peter Lee, Yin Tat Lee, Yuanzhi Li, Scott Lundberg, Harsha Nori, Hamid Palangi, Marco Tulio Ribeiro, Yi Zhang
+# Artificial intelligence (AI) researchers have been developing and refining large language models (LLMs) that exhibit remarkable capabilities across a variety of domains and tasks, challenging our understanding of learning and cognition. The latest model developed by OpenAI, GPT-4, was trained using an unprecedented scale of compute and data. In this paper, we report on our investigation of an early version of GPT-4, when it was still in active development by OpenAI. We contend that (this early version of) GPT-4 is part of a new cohort of LLMs (along with ChatGPT and Google's PaLM for example) that exhibit more general intelligence than previous AI models. We discuss the rising capabilities and implications of these models. We demonstrate that, beyond its mastery of language, GPT-4 can solve novel and difficult tasks that span mathematics, coding, vision, medicine, law, psychology and more, without needing any special prompting. Moreover, in all of these tasks, GPT-4's performance is strikingly close to human-level performance, and often vastly surpasses prior models such as ChatGPT. Given the breadth and depth of GPT-4's capabilities, we believe that it could reasonably be viewed as an early (yet still incomplete) version of an artificial general intelligence (AGI) system. In our exploration of GPT-4, we put special emphasis on discovering its limitations, and we discuss the challenges ahead for advancing towards deeper and more comprehensive versions of AGI, including the possible need for pursuing a new paradigm that moves beyond next-word prediction. We conclude with reflections on societal influences of the recent technological leap and future research directions."""
+# user.initiate_chat(teachable_agent, message=text, clear_history=True)
