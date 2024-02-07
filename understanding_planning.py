226b768ad94ed311a5d5d3f01a92c4a64a3c13e0
SHIV-anna <shivshankar07123@gmail.com>
2024-02-07 19:52:02 +0530

    added print statements

--- understanding_planning.py
+++ understanding_planning.py
@@ -35,12 +35,6 @@ planner_user = autogen.UserProxyAgent(
     human_input_mode="NEVER",
 )
 
-def ask_planner(message):
-    planner_user.initiate_chat(planner, message=message)
-    # return the last message received from the planner
-    return planner_user.last_message()["content"]
-
-
 
 # create an AssistantAgent instance named "assistant"
 assistant = autogen.AssistantAgent(
@@ -50,22 +44,22 @@ assistant = autogen.AssistantAgent(
         "timeout": 600,
         "cache_seed": 42,
         "config_list": config_list,
-        "functions": [
-            {
-                "name": "ask_planner",
-                "description": "ask planner to: 1. get a plan for finishing a task, 2. verify the execution result of the plan and potentially suggest new plan.",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "message": {
-                            "type": "string",
-                            "description": "question to ask planner. Make sure the question include enough context, such as the code and the execution result. The planner does not know the conversation between you and the user, unless you share the conversation with the planner.",
-                        },
-                    },
-                    "required": ["message"],
-                },
-            },
-        ],
+        # "functions": [
+        #     {
+        #         "name": "ask_planner",
+        #         "description": "ask planner to: 1. get a plan for finishing a task, 2. verify the execution result of the plan and potentially suggest new plan.",
+        #         "parameters": {
+        #             "type": "object",
+        #             "properties": {
+        #                 "message": {
+        #                     "type": "string",
+        #                     "description": "question to ask planner. Make sure the question include enough context, such as the code and the execution result. The planner does not know the conversation between you and the user, unless you share the conversation with the planner.",
+        #                 },
+        #             },
+        #             "required": ["message"],
+        #         },
+        #     },
+        # ],
     }
 )
 
@@ -76,12 +70,45 @@ user_proxy = autogen.UserProxyAgent(
     max_consecutive_auto_reply=10,
     # is_termination_msg=lambda x: "content" in x and x["content"] is not None and x["content"].rstrip().endswith("TERMINATE"),
     code_execution_config={"work_dir": "planning"},
-    function_map={"ask_planner": ask_planner},
+    # function_map={"ask_planner": ask_planner},
 )
 
+@user_proxy.register_for_execution()
+@assistant.register_for_llm(description="ask planner to: 1. get a plan for finishing a task, 2. verify the execution result of the plan and potentially suggest new plan.")
+def ask_planner(message):
+    planner_user.initiate_chat(planner, message=message)
+    # return the last message received from the planner
+    return planner_user.last_message()["content"]
 
+
+CurrencySymbol = Literal["USD", "EUR"]
+
+
+def exchange_rate(base_currency: CurrencySymbol, quote_currency: CurrencySymbol) -> float:
+    if base_currency == quote_currency:
+        return 1.0
+    elif base_currency == "USD" and quote_currency == "EUR":
+        return 1 / 1.1
+    elif base_currency == "EUR" and quote_currency == "USD":
+        return 1.1
+    else:
+        raise ValueError(f"Unknown currencies {base_currency}, {quote_currency}")
+
+@user_proxy.register_for_execution()
+@assistant.register_for_llm(description="Currency exchange calculator.")
+def currency_calculator(
+    base_amount: Annotated[float, "Amount of currency in base_currency"],
+    base_currency: Annotated[CurrencySymbol, "Base currency"] = "USD",
+    quote_currency: Annotated[CurrencySymbol, "Quote currency"] = "EUR",
+) -> str:
+    quote_amount = exchange_rate(base_currency, quote_currency) * base_amount
+    return f"{quote_amount} {quote_currency}"
+
+print("user_proxy.function_map",user_proxy.function_map)
+print("assistant.llm_config['tools'][0]['function']",assistant.llm_config['tools'][0]['function'])
+print()
 # the assistant receives a message from the user, which contains the task description
 user_proxy.initiate_chat(
     assistant,
-    message="""What date is today? Compare the year-to-date gain for META and TESLA.""",
+    message="How much is 123.45 USD in EUR?",
 )
