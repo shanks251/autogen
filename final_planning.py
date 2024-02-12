b3f9a6b7127278128c6934d1833b4fce08ac0e1b
SHIV-anna <shivshankar07123@gmail.com>
2024-02-12 20:55:48 +0530

    added print statements

--- final_planning.py
+++ final_planning.py
@@ -36,34 +36,6 @@ def termination_msg(x):
 
 print("LLM models: ", [config_list[i]["model"] for i in range(len(config_list))])
 
-# Reset agents
-def reset_agents(agents):
-    for agent in agents:
-        agent.reset()
-
-# Start chat function
-def start_chat(agents, problem, llm_config):
-    reset_agents(agents)
-    groupchat = autogen.GroupChat(
-        agents=agents,
-        messages=[],
-        max_round=20,
-        speaker_selection_method="auto",
-        allow_repeat_speaker=False,
-    )
-
-    manager_llm_config = llm_config.copy()
-    # manager_llm_config.pop("functions")
-    manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=manager_llm_config)
-
-    # Start chatting with the boss as this is the user proxy agent.
-    boss.initiate_chat(
-        manager,
-        message=PROBLEM,
-    )
-
-# Define the problem statement
-PROBLEM = "What are the GDP figures for the USA and Germany? Additionally, determine which country has the higher GDP and output GDP in their respective national currencies. Output final answer of each sub questions as one final answer."
 
 # Define agents
 pm = autogen.AssistantAgent(
@@ -79,52 +51,6 @@ writer = autogen.AssistantAgent(
     # the default system message of the AssistantAgent is overwritten here
     system_message="You are a movie writer. Your responsible for crafting compelling narratives and dialogue for given movies description."
 )
-
-CurrencySymbol = Literal["USD", "EUR"]
-
-
-def exchange_rate(base_currency: CurrencySymbol, quote_currency: CurrencySymbol) -> float:
-    if base_currency == quote_currency:
-        return 1.0
-    elif base_currency == "USD" and quote_currency == "EUR":
-        return 1 / 1.1
-    elif base_currency == "EUR" and quote_currency == "USD":
-        return 1.1
-    else:
-        raise ValueError(f"Unknown currencies {base_currency}, {quote_currency}")
-
-# Define currency calculator function
-def currency_calculator(
-    base_amount: Annotated[float, "Amount of currency in base_currency"],
-    base_currency: Annotated[CurrencySymbol, "Base currency"] = "USD",
-    quote_currency: Annotated[CurrencySymbol, "Quote currency"] = "EUR",
-) -> str:
-    quote_amount = exchange_rate(base_currency, quote_currency) * base_amount
-    return f"{quote_amount} {quote_currency}"
-
-
-currency_aid = autogen.AssistantAgent(
-    name="currency_assistant",
-    system_message="Suggest currency of given countries and convert it. For currency conversion tasks, only use the functions you have been provided with. Reply TERMINATE when the task is done.",
-    llm_config={"timeout": 60, 
-                "cache_seed": 42, 
-                "config_list": config_list, 
-                "temperature": 0,
-                "functions":[
-                    {
-                        'description': 'Currency exchange calculator.', 
-                        'name': 'currency_calculator', 
-                        'parameters': {
-                            'type': 'object', 
-                            'properties': {
-                                'base_amount': {'type': 'number', 'description': 'Amount of currency in base_currency'}, 
-                                'base_currency': {'enum': ['USD', 'EUR'], 'type': 'string', 'default': 'USD', 'description': 'Base currency'}, 
-                                'quote_currency': {'enum': ['USD', 'EUR'], 'type': 'string', 'default': 'EUR', 'description': 'Quote currency'}}, 
-                            'required': ['base_amount']}
-                    }
-                ]
-            },
-    )
 
 planner = autogen.AssistantAgent(
     name="planner",
@@ -170,12 +96,51 @@ planning_assistant = autogen.AssistantAgent(
         ],
     },
 )
+CurrencySymbol = Literal["USD", "EUR"]
 
-retriever = autogen.AssistantAgent(
-    name="retriever",
-    system_message="Assistant who has extra content retrieval power for solving difficult problems. For retriever tasks, only use the functions you have been provided with",
-    llm_config={"timeout": 600, "cache_seed": 42, "config_list": config_list},
-)
+
+def exchange_rate(base_currency: CurrencySymbol, quote_currency: CurrencySymbol) -> float:
+    if base_currency == quote_currency:
+        return 1.0
+    elif base_currency == "USD" and quote_currency == "EUR":
+        return 1 / 1.1
+    elif base_currency == "EUR" and quote_currency == "USD":
+        return 1.1
+    else:
+        raise ValueError(f"Unknown currencies {base_currency}, {quote_currency}")
+
+# Define currency calculator function
+def currency_calculator(
+    base_amount: Annotated[float, "Amount of currency in base_currency"],
+    base_currency: Annotated[CurrencySymbol, "Base currency"] = "USD",
+    quote_currency: Annotated[CurrencySymbol, "Quote currency"] = "EUR",
+) -> str:
+    quote_amount = exchange_rate(base_currency, quote_currency) * base_amount
+    return f"{quote_amount} {quote_currency}"
+
+
+currency_aid = autogen.Agent(
+    name="currency_assistant",
+    system_message="Suggest currency of given countries and convert it. For currency conversion tasks, only use the functions you have been provided with. Reply TERMINATE when the task is done.",
+    llm_config={"timeout": 60, 
+                "cache_seed": 42, 
+                "config_list": config_list, 
+                "temperature": 0,
+                "functions":[
+                    {
+                        'description': 'Currency exchange calculator.', 
+                        'name': 'currency_calculator', 
+                        'parameters': {
+                            'type': 'object', 
+                            'properties': {
+                                'base_amount': {'type': 'number', 'description': 'Amount of currency in base_currency'}, 
+                                'base_currency': {'enum': ['USD', 'EUR'], 'type': 'string', 'default': 'USD', 'description': 'Base currency'}, 
+                                'quote_currency': {'enum': ['USD', 'EUR'], 'type': 'string', 'default': 'EUR', 'description': 'Quote currency'}}, 
+                            'required': ['base_amount']}
+                    }
+                ]
+            },
+    )
 
 boss = autogen.UserProxyAgent(
     name="Boss",
@@ -188,7 +153,7 @@ boss = autogen.UserProxyAgent(
 )
 
 boss_aid = RetrieveUserProxyAgent(
-    name="Boss_assistant",
+    name="Boss_Assistant",
     is_termination_msg=termination_msg,
     system_message="Assistant who has extra content retrieval power for solving difficult problems.",
     human_input_mode="NEVER",
@@ -210,58 +175,88 @@ boss_aid = RetrieveUserProxyAgent(
     code_execution_config=False,  # we don't want to execute code in this case.
 )
 
-def retrieve_content(message, n_results=1):
-        boss_aid.n_results = n_results  # Set the number of results to be retrieved.
-        # Check if we need to update the context.
-        update_context_case1, update_context_case2 = boss_aid._check_update_context(message)
-        if (update_context_case1 or update_context_case2) and boss_aid.update_context:
-            boss_aid.problem = message if not hasattr(boss_aid, "problem") else boss_aid.problem
-            _, ret_msg = boss_aid._generate_retrieve_user_reply(message)
-        else:
-            ret_msg = boss_aid.generate_init_message(message, n_results=n_results)
-        return ret_msg if ret_msg else message
+# def retrieve_content(message, n_results=1):
+#         boss_aid.n_results = n_results  # Set the number of results to be retrieved.
+#         # Check if we need to update the context.
+#         update_context_case1, update_context_case2 = boss_aid._check_update_context(message)
+#         if (update_context_case1 or update_context_case2) and boss_aid.update_context:
+#             boss_aid.problem = message if not hasattr(boss_aid, "problem") else boss_aid.problem
+#             _, ret_msg = boss_aid._generate_retrieve_user_reply(message)
+#         else:
+#             ret_msg = boss_aid.generate_init_message(message, n_results=n_results)
+#         return ret_msg if ret_msg else message
     
-boss_aid.human_input_mode = "NEVER"
-llm_config_functions = {
-        "functions": [
-            {
-                "name": "retrieve_content",
-                "description": "retrieve content for code generation and question answering.",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "message": {
-                            "type": "string",
-                            "description": "Refined message which keeps the original meaning and can be used to retrieve content for code generation and question answering.",
-                        }
-                    },
-                    "required": ["message"],
-                },
-            },
-        ],
-        "config_list": config_list,
-        "timeout": 60,
-        "cache_seed": 42,
-    }
+# boss_aid.human_input_mode = "NEVER"
+# llm_config_functions = {
+#         "functions": [
+#             {
+#                 "name": "retrieve_content",
+#                 "description": "retrieve content for code generation and question answering.",
+#                 "parameters": {
+#                     "type": "object",
+#                     "properties": {
+#                         "message": {
+#                             "type": "string",
+#                             "description": "Refined message which keeps the original meaning and can be used to retrieve content for code generation and question answering.",
+#                         }
+#                     },
+#                     "required": ["message"],
+#                 },
+#             },
+#         ],
+#         "config_list": config_list,
+#         "timeout": 60,
+#         "cache_seed": 42,
+#     }
 
-for agent in [retriever]:
-    # register functions for all agents.
-    # update llm_config for assistant agents.
-    agent.llm_config.update(llm_config_functions)
+# for agent in [retriever]:
+#     # register functions for all agents.
+#     # update llm_config for assistant agents.
+#     agent.llm_config.update(llm_config_functions)
 
 
-for agent in [boss, retriever]:
-    # register functions for all agents.
-    agent.register_function(
-        function_map={
-            "retrieve_content": retrieve_content,
-        }
-    )
+# for agent in [boss, retriever]:
+#     # register functions for all agents.
+#     agent.register_function(
+#         function_map={
+#             "retrieve_content": retrieve_content,
+#         }
+#     )
 
 
 print("********printing agent tool********")
 print(f"{currency_aid.name}_tools_function: ,{currency_aid.llm_config['functions']}")
-print(f"{retriever.name}_tools_function: ,{retriever.llm_config['functions']}")
+# print(f"{retriever.name}_tools_function: ,{retriever.llm_config['functions']}")
+
+# Reset agents
+def reset_agents(agents):
+    for agent in agents:
+        agent.reset()
+
+# Start chat function
+def start_chat(agents, problem, llm_config):
+    reset_agents(agents)
+    groupchat = autogen.GroupChat(
+        agents=agents,
+        messages=[],
+        max_round=20,
+        speaker_selection_method="auto",
+        allow_repeat_speaker=False,
+    )
+
+    manager_llm_config = llm_config.copy()
+    # manager_llm_config.pop("functions")
+    manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=manager_llm_config)
+
+    # Start chatting with the boss as this is the user proxy agent.
+    agents[0].initiate_chat(
+        manager,
+        message=problem,
+    )
+
+# Define the problem statement
+PROBLEM = "What are the GDP figures for the USA and Germany? Additionally, determine which country has the higher GDP and output GDP in their respective national currencies. Output final answer of each sub questions as one final answer."
+
 
 # Start the chat
-start_chat([boss, currency_aid, retriever], PROBLEM, {"config_list": config_list})
+start_chat([boss_aid, currency_aid], PROBLEM, {"config_list": config_list})
